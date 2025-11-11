234d11f971ee6ab40d83ab4237c2b734d94b0ed6
NHIHU3110 <nhiht234112e@st.uel.edu.vn>
2025-11-11 22:12:06 +0700

    “Cart”

--- NAILISTAS/filechinhnalistas/NAILISTAS---WEBSITE-main/Product/js/product.js
+++ NAILISTAS/filechinhnalistas/NAILISTAS---WEBSITE-main/Product/js/product.js
@@ -444,15 +444,6 @@ if (loginForm) {
     showOverlayThenNavigate(HOMEPAGE_URL);
   });
 }
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/68fd00ab603401195169ddbc/1j8e4l8i4';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
 // Hiệu ứng load trang khi mới vào
 window.addEventListener('load', () => {
   document.body.classList.add('fade-in');
--- NAILISTAS/filechinhnalistas/NAILISTAS---WEBSITE-main/Service/service.js
+++ NAILISTAS/filechinhnalistas/NAILISTAS---WEBSITE-main/Service/service.js
@@ -610,3 +610,14 @@ document.querySelectorAll('a[href]').forEach(link => {
     }, 800);
   });
 });
+
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/68fd00ab603401195169ddbc/1j8e4l8i4';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
